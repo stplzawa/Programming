42d6fd526c44bc4d2e1e4b48c8f6745f8ee62fb0
stplzawa <55458936+stplzawa@users.noreply.github.com>
2019-12-24 22:41:04 +0300

    Add files via upload

---
+++ Zadanie1.cpp
@@ -0,0 +1,12 @@
+﻿// Zadanie1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Результат вычисления 2+2*2=" << 2 + 2 * 2 << endl;
+	
+	return 0;
+}
