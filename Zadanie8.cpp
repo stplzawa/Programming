674b2990c2b420d6e9c6012308c81d5ceaf4d4c8
backtrack <55458936+stplzawa@users.noreply.github.com>
2020-01-16 01:31:30 +0300

    Add files via upload

---
+++ Zadanie8.cpp
@@ -0,0 +1,33 @@
+﻿#include <math.h>>
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double a, b, c;
+	char x; 
+	cout << "Введите выражение, используя следующие символы-знаки:'*,/,+,-'й" << "\n";
+	cin >> a >> x >> b;
+	switch (x) {
+	case '*': c = a * b;
+		cout << "Ответ = " << c << endl;
+		break;   
+	case '/': c = a / b;
+		if (b == 0) {
+			cout << "Ошибка. На 0 делить нельзя" << "\n";
+		}
+		cout << "Ответ = " << c << endl;
+		break;
+	case '+': c = a + b;
+		cout << "Ответ = " << c << endl;
+		break;
+	case '-': c = a - b;
+		cout << "Ответ = " << c << endl;
+		break;
+	default:  
+		cout << "Ошибка. Введите заданное действие: *, /, + , -.";
+}
+	return 0;
+}
+
+
