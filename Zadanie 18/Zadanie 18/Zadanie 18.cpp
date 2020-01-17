1cc1a6252e5906c2a5a61d5b9ae6dcc9819e23a8
backtrack <55458936+stplzawa@users.noreply.github.com>
2020-01-17 21:57:58 +0300

    Add files via upload

---
+++ Zadanie 18/Zadanie 18/Zadanie 18.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+	
+using namespace std;
+
+const int N = 20 ;
+
+int main()
+{
+	srand(time(nullptr));
+	int mass[N], max, min;
+	setlocale(LC_ALL, "RUS");
+	for (int r = 0; r < N; r++)
+	{
+		mass[r] = rand() % 202 - 101;
+		cout << mass[r]<< " ";
+	}
+	cout << endl;
+
+	max = mass[0];
+	min = mass[0];
+	for (int r = 1; r < N; r++)
+	{
+		if (max < mass[r]) max = mass[r];
+		if (min > mass[r]) min = mass[r];
+	}
+	cout << "Min: " << min << endl;
+	cout << "Max: " << max << endl;
+
+	return 0;
+}
