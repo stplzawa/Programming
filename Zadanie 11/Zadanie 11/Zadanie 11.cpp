dd6c1353c4a63c3aa05404183397481c5ff45789
backtrack <55458936+stplzawa@users.noreply.github.com>
2020-01-17 21:55:35 +0300

    Add files via upload

---
+++ Zadanie 11/Zadanie 11/Zadanie 11.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> 
+using namespace std;
+
+void faktorial(int numb)
+{
+	int rezult = 1;
+	for (int i = 1; i <= numb; i++) 
+		rezult *= i;
+	cout <<"Факториал= " << numb << "! = " << rezult << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale (LC_ALL, "RUS");
+	int chislo; 
+	cout << "Введите число: ";
+	cin >> chislo;
+	faktorial(chislo); 
+	return 0;
+}
